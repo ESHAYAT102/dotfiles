3f7e7232ced3838539dd08125dc27916e9beb6d3
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2026-02-27 22:26:18 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -116,6 +116,9 @@ mv local/share/omarchy/default/hypr/bindings/utilities.conf ~/.local/share/omarc
 mv local/share/omarchy/bin/omarchy-menu ~/.local/share/omarchy/bin/omarchy-menu
 chmod +x ~/.local/share/omarchy/bin/omarchy-menu
 
+# TMUX
+mv config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+
 # XCompose
 mv XCompose ~/.XCompose
 mv local/share/omarchy/default/xcompose ~/.local/share/omarchy/default/xcompose
