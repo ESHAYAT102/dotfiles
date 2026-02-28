f40f0b3f1a4646fe3d1b7387501e0f4cb60d5fdd
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2026-03-01 01:06:18 +0600

    Delete config/hypr/scripts/layout.sh

--- config/hypr/scripts/layout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CURRENT_LAYOUT=$(hyprctl getoption general:layout | grep "str:" | awk '{print $2}' | tr -d '"')
-
-if [ "$CURRENT_LAYOUT" = "scrolling" ]; then
-    hyprctl dispatch layoutmsg "$@"
-fi
