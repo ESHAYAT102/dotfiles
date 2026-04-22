1d402ebb13c26815c3705beaf69d4f1cad841ca4
Eshayat Al-Wasiu <133668606+ESHAYAT102@users.noreply.github.com>
2026-04-22 21:26:13 +0600

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,9 +8,9 @@ set -gx PATH $PATH /home/esh/.spicetify
 
 set -gx PATH $HOME/.local/bin $PATH
 
-alias ss "ssh -p 23231 eshayat@homelab"
+# alias ss "ssh -p 23231 eshayat@homelab"
 
-alias s "mosh eshayat@homelab"
+# alias s "mosh eshayat@homelab"
 
 alias q exit
 
