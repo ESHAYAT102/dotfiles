6b2fb2345b7834fd409fbb5da5e5dfd76e437630
Eshayat Al-Wasiu <eshayat.wasiu@gmail.com>
2026-08-17 06:48:29 +0600

    Spotify: autostart + clean toggle_special dispatcher

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,6 +1,7 @@
 -- Extra autostart processes.
 o.launch_on_start("ollama serve")
 o.launch_on_start("vicinae server --replace")
+o.launch_on_start("spotify")
 
 hl.on("hyprland.start", function()
   -- Slow app launch fix -- set systemd vars before starting session services.
