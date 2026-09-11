33fa966cbe3ca2c4abd2e81c6175ddf25ff91216
Eshayat Al-Wasiu <eshayat.wasiu@gmail.com>
2026-09-12 01:52:00 +0600

    👀

--- config/hypr/bindings.lua
+++ config/hypr/bindings.lua
@@ -64,7 +64,7 @@ bind("code:248", "Agent", "omarchy-agent")
 
 bind("SUPER + SHIFT + RETURN", "Alternative Terminal", "terax")
 bind("SUPER + E", "Yazi", "uwsm app -- $TERMINAL -e yazi")
-bind("SUPER + SHIFT + E", "Flea", "flea")
+bind("SUPER + SHIFT + E", "File manager", "uwsm app -- nautilus --new-window")
 bind("SUPER + W", "Browser", "zen-browser")
 bind("SUPER + SHIFT + W", "Private Browser", "zen-browser --private-window")
 bind("SUPER + SHIFT + R", "Activity", "uwsm app -- $TERMINAL -e btop")
